00764d1a78ee84bd0a2e8d36035f54ccee92a7eb
Chris <coluccioc@gmail.com>
2024-07-20 13:49:34 -0400

    Updated comments

--- src/testing.py
+++ src/testing.py
@@ -1,21 +1,19 @@
-import os
-from skimage.io import imread
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
-from sklearn.svm import SVC
-import preprocess
 import pickle
 import train
 from sklearn.metrics import accuracy_score
 
+# Grab the best estimator from the training class
 best_estimator = train.grid_search.best_estimator_
 
+# Make predications based on the testing set
 y_prediction = best_estimator.predict(train.x_test)
 
+# Determine Accuracy Score
 accuracy = accuracy_score(y_prediction, train.y_test)
 
+# Print to review accuracy
 print(f"{str(accuracy*100)}% of images were correctly classified.")
 
+# Save model
 pickle.dump(best_estimator, open('./model.p', 'wb'))
 
